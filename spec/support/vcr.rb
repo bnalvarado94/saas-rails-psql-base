e9d27d075984114d0c3ff13160759eb608aff8e6
bnalvarado94 <bnalvarado94@gmail.com>
2026-05-07 14:31:25 -0600

    update vcr configuration

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,7 +5,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.record = :new_episodes
+  config.default_cassette_options = { record: :new_episodes }
   config.ignore_localhost = true
 end
 
